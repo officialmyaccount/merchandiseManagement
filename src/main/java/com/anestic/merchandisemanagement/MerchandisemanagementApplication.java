79dbd18f60caa89a5f33ff4112df4de87ac32797
officialmyaccount <bicebc@gmail.com>
2023-07-28 16:46:19 +0900

    first commit

---
+++ src/main/java/com/anestic/merchandisemanagement/MerchandisemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.anestic.merchandisemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchandisemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchandisemanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anestic/merchandisemanagement/MerchandisemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anestic.merchandisemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchandisemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
